e585be86e9e2916d5cf900c414c544d064445284
makkalot <makkalot@gmail.com>
2014-09-27 09:44:33 +0300

    python packaging sutff

---
+++ enlivepy/README.txt
@@ -0,0 +1,37 @@
+==================================
+enlivepy
+==================================
+
+enlivepy provides such and such and so and so. You might find
+it most useful for tasks involving <x> and also <y>. Typical usage
+often looks like this::
+
+    #!/usr/bin/env python
+
+    import enlivepy
+
+
+
+A word on Sphinx
+================
+Paragraphs are separated by blank lines. *Italics*, **bold**,
+and ``monospace`` look like this.
+
+Lists look like this:
+
+* First
+
+* Second. Can be multiple lines
+  but must be indented properly.
+
+A Sub-Section
+-------------
+
+Numbered lists look like you'd expect:
+
+1. hi there
+
+2. must be going
+
+Urls are http://like.this and links can be
+written `like this <http://www.example.com/foo/bar>`_.
---
+++ enlivepy/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+""" enlivepy setup.py script """
+
+
+# system
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+from os.path import join, dirname
+
+
+setup(
+    name="enlivepy",
+    version='0.1.0',
+    description='Html Tranformation Library',
+    author='makkalot',
+    author_email='makkalot@gmail.com',
+    packages=['enlivepy','enlivepy.test'],
+    url='https://github.com/makkalot/enlivepy',
+    long_description=open('README.txt').read(),
+    install_requires=[''],
+    test_suite='enlivepy.test',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python',
+      ],
+)
