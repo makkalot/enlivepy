7f9093b877915a37aef0314d8d262c830a9b52ac
makkalot <makkalot@gmail.com>
2014-10-06 15:05:45 +0300

    models for example app

--- enlivepy/example/enlivetest/models.py
+++ enlivepy/example/enlivetest/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class TodoItem(models.Model):
+    """
+    Simple todo item here
+    """
+    name = models.CharField(max_length=100)
+    done = models.BooleanField(default=False)
+
+
+    def __unicode__(self):
+        return u"-".join([self.name, str(self.done)])
