d2984cf988c5e4079843e8f9d057f409edf5f865
makkalot <makkalot@gmail.com>
2014-10-06 15:06:08 +0300

    viees and urlsconfs for todoapp

--- enlivepy/example/example/urls.py
+++ enlivepy/example/example/urls.py
@@ -6,12 +6,18 @@ from django.conf.urls.static import static
 
 from enlivetest.views import HomePageView
 from enlivetest.views import TodoIndexView
-
+from enlivetest.views import TodoListView, TodoDeleteView, TodoToggleUpdateView, TodoDeleteBulkView
 
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', HomePageView.as_view(), name='home'),
     url(r'^index/', TodoIndexView.as_view(), name="todo_index"),
+    url(r'^list/(?P<done>\w+)/', TodoListView.as_view(), name="todo_index_filter"),
+    url(r'^list/', TodoListView.as_view(), name="todo_index"),
+    url(r'^todo/delele/completed/$', TodoDeleteBulkView.as_view(), name="todo_delete_completed"),
+    url(r'^todo/delele/(?P<pk>[0-9]+)/$', TodoDeleteView.as_view(), name="todo_delete"),
+    url(r'^todo/update/(?P<pk>[0-9]+)/$', TodoToggleUpdateView.as_view(), name="todo_update"),
+
 
     url(r'^admin/', include(admin.site.urls)),
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
